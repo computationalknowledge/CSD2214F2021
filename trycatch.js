ca17e8433bf9f07c3f3836d938d3ea1b2620b2e2
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-09-28 23:35:30 -0400

    Create trycatch.js

---
+++ trycatch.js
@@ -0,0 +1,13 @@
+// try catch
+
+try {
+    var i = 1/a;
+    console.log(i);
+  }
+  catch (exception_var) {
+    console.log("OUCH! that hurt!");
+    console.log(exception_var);
+  }
+  finally {
+    console.log("it took a while but you are finally here!");
+  }
