d6487475390521f45f1a7626f93413a3f37a76f6
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-11-30 11:10:52 -0500

    Update jqueryStarter1.js

--- jqueryStarter1.js
+++ jqueryStarter1.js
@@ -33,6 +33,31 @@
             $("p").css("background-color", "yellow");
          });
       </script>
+
+////////////////////////////////////////////////////////////////////////////////////
+
+<html>
+   <head>
+      <title>The jQuery Example</title>
+      <base href="https://www.tutorialspoint.com" />
+      <script type = "text/javascript" 
+         src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+      </script>
+		
+      <script type = "text/javascript" language = "javascript">
+         $(document).ready(function() {
+            $("#myimg").attr("src", "/jquery/images/jquery.jpg");
+         });
+      </script>
+   </head>
+	
+   <body>
+      <div>
+         <img id = "myimg" src = "/images/jquery.jpg" alt = "Sample image" />
+      </div>
+   </body>
+</html>
+
    </head>
 	
    <body>
