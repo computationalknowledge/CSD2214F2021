0f18d46dc088bd0a083fb872b8a97cdba6828eb5
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-11-30 10:05:08 -0500

    Create jqueryStarter1.js

---
+++ jqueryStarter1.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Peter's Great Web Page</title>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+    </script>
+
+</head>
+
+<body>
+    <H1>Hello!</H1>
+
+</body>
+
+</html>
